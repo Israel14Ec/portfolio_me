7a00231860cea9fd8cc613240866458d8cfcbb8d
Israel14Ec <israel.tuglema14@gmail.com>
2024-12-30 17:30:49 -0500

    Add update problems degins in CardProject

--- src/components/Shared/CardProject/CardProject.tsx
+++ src/components/Shared/CardProject/CardProject.tsx
@@ -9,7 +9,7 @@ type CardProjectProps = {
 
 export function CardProject({ img, title, description, to}: CardProjectProps) {
   return (
-    <Link className=" bg-white rounded-lg p-4 shadow-md font-thin w-80" to={to}>
+    <Link className=" bg-white rounded-lg p-4 shadow-md font-thin w-80 mx-auto" to={to}>
         <div className=" bg-card px-5 pt-5 mb-6">
             <img src={img} alt={`Proyecto ${title}`} className=" h-32 w-full"/>
         </div>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,18 +7,18 @@ export default function Contact() {
     <main className=" bg-white p-5 shadow-md">
         <div className=" w-[70%] py-16 mx-auto">
        
-            <div className=" flex gap-16 items-center justify-center">
-                <div className=" w-1/2">
+            <div className=" flex flex-col md:flex-row gap-16 items-center justify-center">
+                <div className=" md:w-1/2">
                     <h2 className=" text-primary text-4xl font-extrabold">Contacto</h2>
                     <p className=" font-thin text-secondary text-lg mt-10 mb-5">
                     Puedes conectarte conmigo a través de las siguientes plataformas sociales. ¡Estoy disponible para cualquier consulta o colaboración!
                     </p>
                     <ContactIcons className=" text-secondary"/>
                 </div>
-                <div className=" w-1/2">
+                <div className=" md:w-1/2">
                     <Slide direction="right" triggerOnce>
                         <div className=" flex justify-center">
-                            <img src="/logoBlack.png" alt="logo IstDev" className=" w-60 h-60 shadow-lg rounded-full " />
+                            <img src="/logoBlack.png" alt="logo IstDev" className=" w-32 h-32 md:w-60 md:h-60 shadow-lg rounded-full " />
                         </div>
                     </Slide>
                 </div>
